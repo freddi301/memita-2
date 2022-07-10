81c8bdabfcd948ced797ab60fb563e9a80f86873
Frederik Batuna <gobi301@gmail.com>
2022-07-10 18:55:07 +0200

    fix: android

--- packages/mobile-react-native/backend.ts
+++ packages/mobile-react-native/backend.ts
@@ -1,5 +1,5 @@
 import rn_bridge from 'rn-bridge';
-import {createApi, createHyperSwarm} from '@memita-2/core';
+import {createApi} from '@memita-2/core';
 import {createSql} from './components/sql';
 import path from 'path';
 import os from 'os';
@@ -34,9 +34,7 @@ process.on('uncaughtException', (err: Error | string) => {
 });
 
 const sql = createSql();
-const api = createApi(sql, {
-  hyper: createHyperSwarm,
-});
+const api = createApi(sql);
 
 rn_bridge.channel.on('message', ({requestId, method, args}: any) => {
   api.then((impl: any) => {
